a876a58b66891a253b81d11031a90f5a29a514eb
andrei-mikhailovskii <106632366+andrei-mikhailovskii@users.noreply.github.com>
2023-02-14 17:49:20 +0100

    refactored code according to the comments

--- src/main/java/pageobjects/PageObject.java
+++ src/main/java/pageobjects/PageObject.java
@@ -2,7 +2,6 @@ package pageobjects;
 
 import io.appium.java_client.AppiumDriver;
 import java.lang.reflect.Field;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.openqa.selenium.WebElement;
 import pageobjects.nativepageobjects.StartPageObject;
@@ -30,15 +29,12 @@ public class PageObject implements IPageObject {
 
 
     @Override
-    public WebElement getWelement(String weName) {
+    public WebElement getWelement(String weName) throws NoSuchFieldException, IllegalAccessException {
+
         // use reflection technique
-        try {
-            Field field = somePageObject.getClass().getDeclaredField(weName);
-            field.setAccessible(true);
-            return (WebElement) field.get(somePageObject);
-        } catch (NoSuchFieldException | IllegalAccessException exception) {
-            LOGGER.log(Level.WARNING, "Exception", exception);
-        }
-        return null;
+        Field field = somePageObject.getClass().getDeclaredField(weName);
+        field.setAccessible(true);
+        return (WebElement) field.get(somePageObject);
+
     }
 }
